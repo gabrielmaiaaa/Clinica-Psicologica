d783839c013b931ccfe69640a3c01c1ebca4a30f
Maia <gabrielmaia6743@gmail.com>
2024-11-22 15:48:05 -0300

    Salvando no .json
    salvando o relato no json

--- Backend/routers/enviarRelato.js
+++ Backend/routers/enviarRelato.js
@@ -1,18 +1,28 @@
 const mqtt = require('mqtt');
+const fs = require('fs');
+const path = require('path');
+
+const bdPath = path.join(__dirname, '..', 'db', 'relato.json');
+const relatosBD = JSON.parse(fs.readFileSync(bdPath, {encoding: 'utf-8'}));
+
 const client = mqtt.connect('wss://test.mosquitto.org:8081');
 
 const gravidade = 'grave';
 
 client.on('connect', () => {
     console.log('Conectado ao broker');
-    client.subscribe('relatos/enviar'); // topico conectado
+    client.subscribe('relatos/enviar'); // tópico conectado
 });
 
 client.on('message', (topico, message) => {
-    if (topico === 'relatos/enviar') { // verifica se a mensagem é desse tópico
+    if (topico === 'relatos/enviar') { 
         const relato = JSON.parse(message.toString());
-        
-        if(gravidade === 'grave'){
+
+        //Salva user no "banco"
+        relatosBD.push(relato);
+        fs.writeFileSync(bdPath, JSON.stringify(relatosBD, null, 2));
+
+        if (gravidade === 'grave') {
             client.publish(
                 'relatos/notificar',
                 JSON.stringify({ userId: relato.userId, text: relato.text, gravidade }),
@@ -20,6 +30,6 @@ client.on('message', (topico, message) => {
                     console.log(`Relato analisado e enviado: ${relato.userId} com gravidade de ${gravidade}`);
                 }
             );
-        }   
+        }
     }
 });
